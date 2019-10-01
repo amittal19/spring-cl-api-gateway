e21bf48d7af83f7a44e71cd35e26104551783d76
Amit Mittal <amitta26@ms.ds.uhc.com>
2019-10-01 12:53:55 +0530

    first commit

---
+++ src/main/java/com/example/springclapigateway/SpringClApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springclapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClApiGatewayApplication.class, args);
+	}
+
+}
